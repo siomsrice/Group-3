029e72e8bd43e866572a6666385312122d4c903c
Jaime Hanz <jaimehanzs@gmail.com>
2021-11-13 10:33:00 +0800

    update function php
    signup and login works

--- code/functions.php
+++ code/functions.php
@@ -1,32 +1,35 @@
 <?php
 
 function check_login($con){
-    if(isset($_SESSION['user_id'])){ /*check if the value is set*/
+	if(isset($_SESSION['user_id'])){
 
-        $id = $_SESSION['user_id'];
-        $query = "select * from user where user_id = '$id' limit 1";
-        $result = mysqli_query($con, $query);
+		$id = $_SESSION['user_id'];
+		$query = "select * from users where user_id = '$id' limit 1";
 
-        if($result && mysqli_num_rows($result) > 0){ #check if the result is postive
-            $user_data = mysqli_fetch_assoc($result);
-            return $user_data;
-        }
-    }
+		$result = mysqli_query($con,$query);
+		if($result && mysqli_num_rows($result) > 0){
 
-    //if it doesnt exist redirect to login
-    header("Location: login.php");
-    die;
+			$user_data = mysqli_fetch_assoc($result);
+			return $user_data;
+		}
+	}
+
+	//redirect to login
+	header("Location: login.php");
+	die;
 }
 
-function  random_num($length){
-    $text = "";
-    if($length < 5){
-        $length = 5;
-    }
+function random_num($length){
 
-    $len = rand(4, $length);
-    for($i=0; $i < $len; $i++){
-        $text .= rand(0,9);
-    }
-    return $text;
+	$text = "";
+	if($length < 5){
+		$length = 5;
+	}
+
+	$len = rand(4,$length);
+
+	for ($i=0; $i < $len; $i++) { 
+		$text .= rand(0,9);
+	}
+	return $text;
 }
