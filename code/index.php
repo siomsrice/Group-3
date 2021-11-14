124e5b0108510eec3923af9c859bed4c20ac593f
Jaime Hanz <jaimehanzs@gmail.com>
2021-11-14 16:36:11 +0800

    new
    added bunch of error handling, few sample ui

--- code/index.php
+++ code/index.php
@@ -1,30 +1,23 @@
 <?php
-    session_start();
-
-    include("connection.php");
-    include("functions.php");
-
-    $user_data = check_login($con);
+    include_once 'header.php';
 ?>
 
+    <div class="index-intro">
+        <?php
+         if(isset($_SESSION["useruid"])){
+            echo "<p>Hello, ". $_SESSION["useruid"]  ."</p>";
+           
+        }
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PC Builder</title>
-</head>
+        else{
+            echo "<li><a href='signup.php'>Signup</a></li>";
+            echo "<li><a href='login.php'>Login</a></li>";
+        }
+        ?>
 
+        <h1>Main Body</h1>
+    </div>
 
-<body>
-    <a href="logout.php">Logout</a>
-    <h1>Index Page</h1>
-
-    <br>
-    Hello, Username.
-
-
-</body>
-</html>
+<?php
+    include_once 'footer.php';
+?>
--- code/signup.php
+++ code/signup.php
@@ -1,82 +1,57 @@
 <?php
-    session_start();
-
-    include("connection.php");
-    include("functions.php");
-
-    if($_SERVER['REQUEST_METHOD'] == "POST"){
-
-        $user_name = $_POST['user_name'];
-        $password = $_POST['password'];
-        //$email = $_POST['email'];
-
-        if(!empty($user_name) && !empty($password)){
-
-            //save to db
-            $user_id = random_num(20);
-            $query = "insert into users (user_id, user_name, password) value ('$user_id', '$user_name', '$password')";
-
-            mysqli_query($con, $query);
-            header("Location: login.php");
-            die;
-        }   
-        
-        else{
-            echo "Please enter some valid information!";
-        }
-    }   
+    include_once 'header.php';
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Signup</title>
-</head>
-
-<body>
-        <style tyoe="text/css"> /*temporary style*/
-
-            /*for text boxes*/
-            #text{
-                height: 25px;
-                border-radius: 5px;
-                padding: 4px;
-                border: solid thin #aaa;
-                width: 100%; 
-            } 
-
-            #button{
-                padding: 10px;
-                width: 100px;
-                color: white;
-                background-color: lightgrey;
-                border: none;
-            }
-
-            #box{
-                background-color: lightblue;
-                margin: auto;
-                width: 300px;
-                padding: 20px;
-            }
-        </style>
-
-        <div id="box">
-
-                <form method="post">
-                    <div style="font-size: 20px; margin:10px; font-weight:500; color:black">Sign Up</div>
-                    <input id="text" type="text" name="user_name"> <br><br>
-                    <input id="text" type="password" name="password"> <br><br>
-                
-
-                    <input id="button" type="submit" value="Sign Up"> <br><br>
-                    <a href="login.php">Click to Login</a>
-                </form>
-
+    <section class="signup-form">
+        <h2>Sign Up</h2>
+            <div class="signup-form-form">
+                <form action="includes/signup.inc.php" method="post">
+                    <input type="text" name="name" placeholder="full name...">
+                    <input type="text" name="email" placeholder="email...">
+                    <input type="text" name="uid" placeholder="username...">
+                    <input type="password" name="pwd" placeholder="password...">
+                    <input type="password" name="pwdrepeat" placeholder="repeat password...">
+                    <button type="submit" name="submit">Sign Up</button>
+            </form>
         </div>
-</body>
-</html>
+
+        <?php
+        if(isset($_GET["error"])){
+            if ($_GET["error"] == "emptyinput") {
+                echo "<p>Please fill up the fields</p>";
+                
+            }
+
+            else if($_GET["error"] == "invaliduid"){
+                echo "<p>enter a proper username</p>";
+            }
+
+            else if($_GET["error"] == "usernametaken"){
+                echo "<p>username already exist, please try a new one</p>";
+            }
+
+            else if($_GET["error"] == "invalidemail"){
+                echo "<p>enter a proper email</p>";
+            }
+
+            else if($_GET["error"] == "passwordsdontmatch"){
+                echo "<p>password doesnt match</p>";
+            }
+
+            else if($_GET["error"] == "stmtfailed"){ 
+                echo "<p>something went wrong, please try again</p>";
+            }
+
+            else if($_GET["error"] == "none"){ 
+                echo "<p>you have signed up</p>";
+            }
+        }
+
+    ?>
+    </section>
+
+    
+
+<?php
+    include_once 'footer.php';
+?>
