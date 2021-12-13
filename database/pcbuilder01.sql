deba68f460639e28cac414e2186b5f85baad8b7e
Jaime Hanz <jaimehanzs@gmail.com>
2021-12-13 19:09:59 +0800

    updated schema

--- database/pcbuilder01.sql
+++ database/pcbuilder01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2021 at 02:37 PM
+-- Generation Time: Dec 13, 2021 at 12:08 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.30
 
@@ -24,21 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `buy`
+-- Table structure for table `admin`
 --
 
-CREATE TABLE `buy` (
-  `id` int(11) NOT NULL,
-  `item` varchar(256) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `total` decimal(10,2) NOT NULL,
-  `orderDate` datetime NOT NULL,
-  `status` varchar(50) NOT NULL,
-  `customerName` varchar(256) NOT NULL,
-  `customerContact` varchar(256) NOT NULL,
-  `customerEmail` varchar(256) NOT NULL,
-  `customerAddress` varchar(256) NOT NULL
+CREATE TABLE `admin` (
+  `adminId` int(11) NOT NULL,
+  `username` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`adminId`, `username`, `password`, `email`, `date`) VALUES
+(1, 'admin', 'admin', 'admin@gmail.com', '2021-12-12 13:44:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brandcategory`
+--
+
+CREATE TABLE `brandcategory` (
+  `brandID` int(11) NOT NULL,
+  `brandName` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -48,16 +60,33 @@ CREATE TABLE `buy` (
 --
 
 CREATE TABLE `items` (
-  `id` int(11) UNSIGNED NOT NULL,
+  `ItemId` int(11) UNSIGNED NOT NULL,
+  `supplierId` int(11) NOT NULL,
   `itemName` varchar(256) NOT NULL,
   `itemBrand` varchar(256) NOT NULL,
   `itemType` varchar(256) NOT NULL,
   `itemDesc` text NOT NULL,
-  `itemImg` varchar(256) NOT NULL,
-  `itemPrice` decimal(10,2) NOT NULL,
-  `categoryid` int(11) UNSIGNED NOT NULL,
-  `active` varchar(10) NOT NULL,
-  `featured` varchar(10) NOT NULL
+  `itemPrice` float NOT NULL,
+  `itemImg` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `OrderId` int(11) NOT NULL,
+  `usersId` int(11) NOT NULL,
+  `itemId` int(11) NOT NULL,
+  `itemName` varchar(256) NOT NULL,
+  `price` float NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `status` varchar(256) DEFAULT NULL,
+  `orderDate` datetime NOT NULL,
+  `successDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `supplierId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -324,6 +353,23 @@ CREATE TABLE `pma__users` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `supplierId` int(11) NOT NULL,
+  `brandID` int(11) NOT NULL,
+  `Name` varchar(256) NOT NULL,
+  `Email` varchar(256) NOT NULL,
+  `Url` varchar(256) NOT NULL,
+  `Phone` varchar(256) NOT NULL,
+  `Address` text NOT NULL,
+  `Img` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -353,23 +399,39 @@ INSERT INTO `users` (`usersId`, `firstName`, `usersEmail`, `usersUid`, `usersPwd
 (9, 'update', 'update@gmail.com', 'updatetest', '$2y$10$XDRvaaNXqfPI.6W7uPVsKuMwZGbH2lj3//SINhYQZVBoZEPcxLlXa', 0, 'n/a', '', '', NULL, '', 'Agree'),
 (10, 'update123', 'update132@gmail.com', '', '202cb962ac59075b964b07152d234b70', 0, 'n/a', '', '', NULL, '', 'Agree'),
 (12, 'dbeaver', 'dbeaver@gmail.com', 'dbeavs', '$2y$10$hNolWKBvf7/kZv5vQW6knujRI9f8yyqnXOSzi0yJ3VWd0AEecUJuG', 0, 'n/a', '', '', NULL, '', 'Agree'),
-(19, 'firstname', 'email@gmail.com', 'username', '202cb962ac59075b964b07152d234b70', 123, '123', 'Buyer', 'lastname', 'Active', '', 'Agree');
+(19, 'firstname', 'email@gmail.com', 'username4', '202cb962ac59075b964b07152d234b70', 123, '123', 'Buyer', 'lastname', 'Active', '', 'Agree'),
+(20, 'asd', 'gmail@gmail.com', 'testtt', '202cb962ac59075b964b07152d234b70', 123, 'address', 'Buyer', 'zxc', 'Active', '', 'Agree'),
+(21, 'newfirstname1', 'newemail1@gmail.com', 'newsername1', '202cb962ac59075b964b07152d234b70', 12345, 'new address', 'Buyer', 'newlastname1', 'Active', '', 'Agree'),
+(22, 'namename', 'mail@gmail.com', 'useruser', '202cb962ac59075b964b07152d234b70', 123, 'addaddress', 'Buyer', 'sursur', 'Inactive', '', 'Agree'),
+(23, 'mark cedrick', 'markcedrick@gmail.com', 'markcedqt', '202cb962ac59075b964b07152d234b70', 19239821, 'mark address', 'Buyer', 'doria', 'Inactive', '', 'Agree');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `buy`
+-- Indexes for table `admin`
 --
-ALTER TABLE `buy`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`adminId`);
+
+--
+-- Indexes for table `brandcategory`
+--
+ALTER TABLE `brandcategory`
+  ADD PRIMARY KEY (`brandID`);
 
 --
 -- Indexes for table `items`
 --
 ALTER TABLE `items`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`ItemId`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`OrderId`);
 
 --
 -- Indexes for table `pma__bookmark`
@@ -492,6 +554,12 @@ ALTER TABLE `pma__users`
   ADD PRIMARY KEY (`username`,`usergroup`);
 
 --
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`supplierId`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -502,16 +570,28 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `buy`
+-- AUTO_INCREMENT for table `admin`
 --
-ALTER TABLE `buy`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `admin`
+  MODIFY `adminId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `brandcategory`
+--
+ALTER TABLE `brandcategory`
+  MODIFY `brandID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `ItemId` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `OrderId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `pma__bookmark`
@@ -550,10 +630,16 @@ ALTER TABLE `pma__savedsearches`
   MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `supplierId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `usersId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `usersId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
