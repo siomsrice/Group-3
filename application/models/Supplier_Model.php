6ff25d42844405dd09e16f6d1596046b68a8498b
Jaime Hanz <jaimehanzs@gmail.com>
2022-01-09 07:08:57 +0800

    new

--- application/models/Supplier_Model.php
+++ application/models/Supplier_Model.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit ('No direct script access allowed');
 
 class Supplier_model extends CI_Model {
 
-    private $table = "supplier";
+    private $table = "supplier_db";
 
     public function create($formArray){
         $this->db->insert($this->table, $formArray);
@@ -15,18 +15,18 @@ class Supplier_model extends CI_Model {
     }
 
     public function getSupplier($id){
-        $this->db->where('supplierId', $id);
+        $this->db->where('id', $id);
         $supply= $this->db->get($this->table)->row_array();
         return $supply;
     }
 
     public function update($id, $formArray){
-        $this->db->where('supplierId', $id);
+        $this->db->where('id', $id);
         $this->db->update($this->table, $formArray);
     }
 
     public function delete($id){
-        $this->db->where('supplierId', $id);
+        $this->db->where('id', $id);
         $this->db->delete($this->table);
     }
 
@@ -42,5 +42,39 @@ class Supplier_model extends CI_Model {
         $result = $this->db->get()->result_array();
         return $result;
     }
+
+    /* 
+     * Fetch files data from the database 
+     * @param id returns a single record if specified, otherwise all records 
+     */ 
+    public function getRows($id = '')
+    { 
+        $this->db->select('id,sup_name,sup_email,sup_url,sup_phone,file_name'); 
+        $this->db->from('supplier_db'); 
+        if($id)
+        { 
+            $this->db->where('id',$id); 
+            $query = $this->db->get(); 
+            $result = $query->row_array(); 
+        }
+        else
+        { 
+           
+            $query = $this->db->get(); 
+            $result = $query->result_array(); 
+        } 
+        return !empty($result)?$result:false; 
+    } 
+     
+    /* 
+     * Insert file data into the database 
+     * @param array the data for inserting into the table 
+     */ 
+    public function insert($data = array())
+    { 
+        $insert = $this->db->insert_batch($this->table, $data); 
+        return $insert?true:false; 
+    } 
+
 
 }
