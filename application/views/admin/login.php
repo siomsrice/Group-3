977574de9e5b5b3d58d48dcb7e20c843a8b65ef0
Jaime Hanz <jaimehanzs@gmail.com>
2022-02-08 19:11:07 +0800

    Update login.php

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,50 +1,39 @@
-	<!-- Login Start -->
-	<section class="Form my-4 mx-5 w-100 mx-auto text-white">
-        <h1 class="text-center">Admin Panel</h1>
-        <h5 class="text-center">Please Log in to Continue</h5>
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-7 mx-auto bg-dark bg-opacity-50 p-5 mt-5">
-					<h3 class="pb-4 text-center">Login Your Account</h3>
-                    <?=isset($message) ? $message: "";?>
-					<form method="POST" class="">
-						<div class="form-row">
-							<div class="col-12">
-								<input type="text" name="username" placeholder="Email/Username" class="form-control my-3 p-3">
-							</div>
-						</div>
-						<div class="form-row">
-							<div class="col-12">
-								<input type="password" name="password" placeholder="Password" class="form-control my-3 p-3">
-							</div>
-						</div>
-                   
-						<div class="form-row">
-							<div class="col-lg-12 d-flex justify-content-around">
-                                <input type="submit" value ="login" class="mt-3 mb-4 col-3">
-							</div>
-						</div>
+<style>
+    <?php
+    include 'public/style.css';
+    ?>
+</style>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
-					</form>
-				</div>
-			</div>
-		</div>
-	</section>
-    <!-- Login End -->
-
-
-
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-    <!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-</body>
-</html>
+<div class="container">
+    <div class="row">
+        <div class='col-md-3'></div>
+        <div class="col-md-6 loginbox">
+            <div class="login-box well">
+            <form action="" method="POST" class="" id="myForm" style="height:260px" enctype="multipart/form-data">
+                        <legend>Sign In</legend>
+                        <div class="form-group has-feedback">
+                            <label for="username-email">E-mail or Username</label>
+                            <input name="username" id="username-email" placeholder="E-mail or Username" type="text" class="form-control"  />
+                            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                        </div>
+                        <div class="form-group has-feedback">
+                            <label for="password">Password</label>
+                            <input name="password" id="password" value='' placeholder="Password " type="password" class="form-control" />
+                            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                        </div>
+                        <div class="input-group">
+                        </div>
+                        <div class="col-xs-4 loginbutton">
+                            <input type="submit" class="btn btn-primary btn-block btn-flat" value="Login" />
+                        </div>
+                        <div></div>
+                    </form>
+                
+            </div>
+        </div>
+       
+    </div>
